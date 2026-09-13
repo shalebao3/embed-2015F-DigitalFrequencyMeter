6aedff9b7bc608165f4df8aa7f4036b3327862e7
ganlu <133077713+shalebao3@users.noreply.github.com>
2026-09-13 18:18:13 +0800

    ``` feat(tim): 添加TIM2通道2输入捕获功能
    - 配置TIM2通道2的输入捕获功能
    - 更新GPIOA初始化以包含PA1引脚用于TIM2_CH2
    - 在固件ioc文件中添加相应的引脚配置和信号映射
    - 更新定时器初始化函数以配置通道2输入捕获
    - 修正assert_failed函数文档注释描述
    
    此更改扩展了定时器功能，支持双通道输入捕获。
    ```

--- firmware/Core/Src/main.c
+++ firmware/Core/Src/main.c
@@ -473,7 +473,7 @@ void Error_Handler(void)
   * @brief  Reports the name of the source file and the source line number
   *         where the assert_param error has occurred.
   * @param  file: pointer to the source file name
-  * @param  line: source line number
+  * @param  line: assert_param error line source number
   * @retval None
   */
 void assert_failed(uint8_t *file, uint32_t line)
--- firmware/Core/Src/tim.c
+++ firmware/Core/Src/tim.c
@@ -118,6 +118,10 @@ void MX_TIM2_Init(void)
   {
     Error_Handler();
   }
+  if (HAL_TIM_IC_ConfigChannel(&htim2, &sConfigIC, TIM_CHANNEL_2) != HAL_OK)
+  {
+    Error_Handler();
+  }
   /* USER CODE BEGIN TIM2_Init 2 */
 
   /* USER CODE END TIM2_Init 2 */
@@ -251,8 +255,9 @@ void HAL_TIM_Base_MspInit(TIM_HandleTypeDef* tim_baseHandle)
     __HAL_RCC_GPIOA_CLK_ENABLE();
     /**TIM2 GPIO Configuration
     PA0-WKUP     ------> TIM2_CH1
+    PA1     ------> TIM2_CH2
     */
-    GPIO_InitStruct.Pin = GPIO_PIN_0;
+    GPIO_InitStruct.Pin = GPIO_PIN_0|GPIO_PIN_1;
     GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
     GPIO_InitStruct.Pull = GPIO_NOPULL;
     HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
@@ -353,8 +358,9 @@ void HAL_TIM_Base_MspDeInit(TIM_HandleTypeDef* tim_baseHandle)
 
     /**TIM2 GPIO Configuration
     PA0-WKUP     ------> TIM2_CH1
+    PA1     ------> TIM2_CH2
     */
-    HAL_GPIO_DeInit(GPIOA, GPIO_PIN_0);
+    HAL_GPIO_DeInit(GPIOA, GPIO_PIN_0|GPIO_PIN_1);
 
     /* TIM2 interrupt Deinit */
     HAL_NVIC_DisableIRQ(TIM2_IRQn);
